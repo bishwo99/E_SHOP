8309260b8d7883b8267c68a834cf9f996bcdd146
Bishwojit <forcoding55@gmail.com>
2026-09-04 21:02:46 +0600

    Completing Model

--- shop/models.py
+++ shop/models.py
@@ -43,7 +43,58 @@ class Rating(models.Model):
         return f"{self.user.username} - {self.product.name} - {self.rating}"
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    user = models.OneToOneField(on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
+    #total price
+    def get_total_price(self):
+        return sum(item.get_cost() for item in self.items.all()) #Confusion ache ekhane
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} X {self.product.name}"
+    #Item price 
+    def get_cost(self):
+        return self.quantity * self.product.price
+
+class Order(models.Model):
+    STATUS = [
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('canceled' , 'Canceled'),
+    ]
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+    phone = models.CharField(max_length=12)
+    postal_code = models.CharField()
+    city = models.CharField(max_length=100)
+    note = models.TextField()
+    paid = models.BooleanField(default= False)
+    transaction_id = models.CharField(max_length=100)
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+    def get_total_cost(self):
+            return sum(item.get_cost() for item in self.order_items.all())
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete= models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def get_cost(self):
+        return self.quantity * self.product.price
